81b182d600ed9e82fd2c7119c845dcee34d0d6ab
Ritik agarwal <ritik.9869@gmail.com>
2020-10-20 18:46:14 +0530

    updated about us

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -3,19 +3,7 @@ import './Works.css';
 
 function Works(){
     return(<div>
-        <ul>.</ul>
-        <ul>.</ul>
-        <ul>.</ul>
-        <ul>.</ul>
-        <ul>.</ul>
-        <ul>.</ul>
-        <ul>.</ul>
-        <ul>.</ul>
-        <ul>.</ul>
-        <ul>.</ul>
-        <ul>.</ul>
-        <ul>.</ul>
-        <ul>.</ul>
+      
         Works
     </div>)
 }
