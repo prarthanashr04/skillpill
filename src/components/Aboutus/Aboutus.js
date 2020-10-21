7b3155088c7e2dea8a60b5a94d1056c77b09f709
Jatin <adriano23012001@gmail.com>
2020-10-21 17:27:44 +0530

    updated about

--- src/components/Aboutus/Aboutus.js
+++ src/components/Aboutus/Aboutus.js
@@ -5,20 +5,22 @@ function Aboutus(){
     return(<div className = "text1">
       <h1>ABOUT US</h1>
 
-<h3>It all started on 17 June when our mentor Aashish Sir invited us to work on a program. While developing the solution we encountered a lot of problems and in the process of solving those problems we were continuously upgrading ourselves. We got exposure to multiple things and the guidance of Aashish sir always helped us to stick to the path as while exploring things there is a natural tendency of a person to deviate from his main objective. We were working in a team where we collaboratively worked supporting each other solving real problems. Although you can say we were developing a project but it was something special, something more than just a web application. So what was special about it:-
-We were in continuous touch of our clients and were receiving feedbacks
-We were not limited to person’s thinking or perspective of a project and the ideas of team and feedback our clients gave us a the opportunity to try various things during the execution and that phase was full of learning
-We were working with the 23 years of experience of Aashish Sir who at any point was not limiting us to a certain perspective but allowed us to try various things and his guidance allowed us to maintain balance in production and research
-We gave a live presentation online explaining our journey and project thus not only improving our communication skills but motivating and inspiring others from our approach in moving forwards
-We were not working under the burden of developing things to improve our resume but working with an excitement of continuously developing things daily which even fascinates us when we look back
-
-At the same time we realize the importance of working on our basics as well i.e. developing our problem solving approach. So we worked on our core as well continuously by things like competitive programming which add another spice to our routine. Solving problems and encountering errors might be frustrating at times but the whole process was full of learning new things with an excitement of what’s next always there.
-
-We were also analysing ourselves at regular intervals.
-We have daily sync up meetings where we discuss what we did each day and what we should focus on now. This was something like looking in the mirror and realizing and learning from the previous work and using the information to apply it now so that it smoothes our future tasks.
-We had a deep analysis of our work done every two week so that we can see how consistent we are and look back and see where we lacked and what went good etc.
-</h3>
-
+<p>It all started on 17 June when our mentor Aashish Sir invited us to work on a program. While developing the solution we encountered a lot of problems and in the process of solving those problems we were continuously upgrading ourselves. We got exposure to multiple things and the guidance of Aashish sir always helped us to stick to the path as while exploring things there is a natural tendency of a person to deviate from his main objective. We were working in a team where we collaboratively worked supporting each other solving real problems. Although you can say we were developing a project but it was something special, something more than just a web application.</p>
+<ul>
+<div> So what was special about it:-</div>
+    <li>We were in continuous touch of our clients and were receiving feedbacks</li>
+    <li>We were not limited to person’s thinking or perspective of a project and the ideas of team and feedback our clients gave us a the opportunity to try various things during the execution and that phase was full of learning</li>
+    <li>We were working with the 23 years of experience of Aashish Sir who at any point was not limiting us to a certain perspective but allowed us to try various things and his guidance allowed us to maintain balance in production and research</li>
+    <li>We gave a live presentation online explaining our journey and project thus not only improving our communication skills but motivating and inspiring others from our approach in moving forwards</li>
+    <li>We were not working under the burden of developing things to improve our resume but working with an excitement of continuously developing things daily which even fascinates us when we look back</li>
+</ul>
+<p>At the same time we realize the importance of working on our basics as well i.e. developing our problem solving approach. So we worked on our core as well continuously by things like competitive programming which add another spice to our routine. Solving problems and encountering errors might be frustrating at times but the whole process was full of learning new things with an excitement of what’s next always there.</p>
+<p>We were also analysing ourselves at regular intervals.</p>
+<ul>
+    <li>We have daily sync up meetings where we discuss what we did each day and what we should focus on now. </li>
+    <li>This was something like looking in the mirror and realizing and learning from the previous work and using the information to apply it now so that it smoothes our future tasks.</li>
+    <li>We had a deep analysis of our work done every two week so that we can see how consistent we are and look back and see where we lacked and what went good etc.</li>
+</ul>
     </div>)
 }
 
